1b40c0838706230fc3c6eb442aa92df67c647b91
CallMeiota <MainK@KJ>
2024-10-03 01:49:56 +0100

    Updated Item Recipes | Made easier to make.

--- src/main/java/net/mriota/thebox/init/TheBoxModItems.java
+++ src/main/java/net/mriota/thebox/init/TheBoxModItems.java
@@ -7,7 +7,6 @@ package net.mriota.thebox.init;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
-import net.mriota.thebox.item.TapeItem;
 import net.mriota.thebox.item.CardBoardPieceWithoutItem;
 import net.mriota.thebox.item.CardBoardPieceWithPrintItem;
 import net.mriota.thebox.block.display.TheBoxDisplayItem;
@@ -21,7 +20,6 @@ public class TheBoxModItems {
 	public static final DeferredHolder<Item, Item> THE_BOX = REGISTRY.register(TheBoxModBlocks.THE_BOX.getId().getPath(), () -> new TheBoxDisplayItem(TheBoxModBlocks.THE_BOX.get(), new Item.Properties()));
 	public static final DeferredHolder<Item, Item> CARD_BOARD_PIECE_WITHOUT = REGISTRY.register("card_board_piece_without", CardBoardPieceWithoutItem::new);
 	public static final DeferredHolder<Item, Item> CARD_BOARD_PIECE_WITH_PRINT = REGISTRY.register("card_board_piece_with_print", CardBoardPieceWithPrintItem::new);
-	public static final DeferredHolder<Item, Item> TAPE = REGISTRY.register("tape", TapeItem::new);
 	// Start of user code block custom items
 	// End of user code block custom items
 }
